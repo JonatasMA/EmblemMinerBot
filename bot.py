090a4f83643450a2686952056513c3e60bbf47ea
Jonatas <jonatasmacedo7)@hotmail.com>
2021-06-07 12:34:30 -0300

    wip bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import pygetwindow as gw
+# print(gw.getAllTitles())
+leagueClient = gw.getWindowsWithTitle('League of Legends')[0]
+leagueClient.restore()
+# leagueGameClient = gw.getWindowsWithTitle('League of Legends (TM) Client')[0]
+
+# print(leagueClient.width)
+# print(leagueClient.height)
+# print(leagueClient.top)
+# print(leagueClient.left)
